4332459676599a43f5c9ab2c7373b73507a1115d
aldoprogrammer <aldobesma@gmail.com>
2024-01-30 21:53:42 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,5 +78,13 @@ class UserController extends Controller
         return back()->with(['success' => 'Password Changed!']);
     }
 
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+
 
 }
